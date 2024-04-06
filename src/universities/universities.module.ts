310dcc2b48a3a006abcc0f42f08d55fcb8c8d9b7
debkanti <moonnewprocess@yahoo.com>
2024-04-06 14:25:51 +0600

    University entity added

--- src/universities/universities.module.ts
+++ src/universities/universities.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UniversitiesController } from './universities.controller';
 import { UniversitiesService } from './universities.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { University } from './university.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([University])],
   controllers: [UniversitiesController],
-  providers: [UniversitiesService]
+  providers: [UniversitiesService],
 })
 export class UniversitiesModule {}
